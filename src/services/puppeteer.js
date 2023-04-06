9ff97af3cd5358f6bae7fe92520c7cc2882826c6
Aaron McPherson <contactaaronmcpherosn@gmail.com>
2023-04-06 10:17:57 +0700

    dockerfile puppeteer cache env

--- src/services/puppeteer.js
+++ src/services/puppeteer.js
@@ -1,10 +1,17 @@
 import puppeteer from 'puppeteer'
 
+// launch with production config
 export const launchPuppeteer = async (options = {}) => {
   const browser = await puppeteer.launch({
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu',
     ],
     ...options,
   })
